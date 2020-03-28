911e98fb9581e9af1e909eab5e3f198dab7204b4
Bade, Narendra (Contractor) <narendrabade@gmail.com>
2020-03-27 23:38:43 -0400

    first commit

---
+++ Basic1/ContentView.swift
@@ -0,0 +1,21 @@
+t//
+//  ContentView.swift
+//  Basic1
+//
+//  Created by NARENDRA Carnival on 3/27/20.
+//  Copyright © 2020 Carnival. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
